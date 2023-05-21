42ef47c22c1a8c986c80e2f8abc9d6a831c27a7a
Jaivik <jaivikmoradiya2001@gmail.com>
2023-05-21 13:44:10 +0530

    Initial commit

---
+++ lib/Screen/model.dart
@@ -0,0 +1,44 @@
+List Allproduct = [
+
+  {
+    "image": "assets/images/11.jpg",
+    "name": "belt",
+    "price": "\$22",
+  },
+  {
+    "image": "assets/images/12.jpg",
+    "name": "school beg",
+    "price": "\$55",
+  },
+  {
+    "image": "assets/images/13.jpg",
+    "name": "Goggals",
+    "price": "\$10",
+  },
+  {
+    "image": "assets/images/14.jpg",
+    "name": "airpods",
+    "price": "\$34",
+  },
+  {
+    "image": "assets/images/15.png",
+    "name": "mobile",
+    "price": "\$58",
+  },
+
+  {
+    "image": "assets/images/16.jpg",
+    "name": "Smart whach",
+    "price": "\$16",
+  },
+  {
+    "image": "assets/images/17.jpg",
+    "name": "T-shirt",
+    "price": "\$32",
+  },
+  {
+    "image": "assets/images/18.png",
+    "name": "pent",
+    "price": "\$33",
+  }
+];
